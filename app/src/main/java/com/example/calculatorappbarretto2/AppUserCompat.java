99eadbe9abf32f6f9305472c1703f9bc8919b5e1
ryanforschool <barretto5736@students.d211.org>
2023-09-08 08:17:31 -0500

    functionality
    added functionality to buttons and return output

---
+++ app/src/main/java/com/example/calculatorappbarretto2/AppUserCompat.java
@@ -0,0 +1,17 @@
+package com.example.calculatorappbarretto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AppUserCompat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_user_compat);
+
+        Intent intent = getIntent();
+    }
+}
--- app/src/main/java/com/example/calculatorappbarretto2/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbarretto2/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.calculatorappbarretto2;
 
+import static java.lang.Float.parseFloat;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,14 +21,41 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View view){
 
-        EditText number1 = (EditText) findViewById(R.id.inputNumber1);
+        EditText number1 = findViewById(R.id.inputNumber1);
         EditText number2 = (EditText) findViewById(R.id.inputNumber2);
+
+        float number1_val = parseFloat(number1.getText().toString());
+        float number2_val = parseFloat(number2.getText().toString());
+
+        float result = 0;
+
+        String output = "";
 
         int buttonId = view.getId();
 
         if (buttonId == R.id.buttonAddition){
-           //\ int result = number1 + number2;
-            Log.i("ryan", "" + number1.getText() + number2.getText());
+            result = number1_val + number2_val;
+            output = "" + number1_val + " + " + number2_val + " = " + result;
         }
+        else if (buttonId == R.id.buttonSubstraction){
+            result = number1_val - number2_val;
+            output = "" + number1_val + " - " + number2_val + " = " + result;
+        }
+        else if (buttonId == R.id.buttonMultiplication){
+            result = number1_val * number2_val;
+            output = "" + number1_val + " x " + number2_val + " = " + result;
+        }
+        else if (buttonId == R.id.buttonDivision){
+            result = number1_val / number2_val;
+            output = "" + number1_val + " / " + number2_val + " = " + result;
+        }
+
+        Log.i("ryanbar", "" + result);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        TextView outputTextView = findViewById(R.id.textView4);
+
+        outputTextView.setText(output);
     }
 }
